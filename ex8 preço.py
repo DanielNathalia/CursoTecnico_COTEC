fd3935c74875fd08631409b6bee9d40cdd4ef99b
DanielNathalia <77645774+DanielNathalia@users.noreply.github.com>
2024-05-17 13:57:07 -0300

    Update ex8 preço.py

--- ex8 preço.py
+++ ex8 preço.py
@@ -1,9 +1,22 @@
-n1= float(input('digite o preço do produto:'))
-n2= 5
-m= n2 * n1
-print('{} * {} = {}'.format(n2, n1, m))
-d= m / 100
-print('{} / {} = {}'.format(m,100, d))
-sub= n1 - d 
-print('{} - {} = {}'.format(n1,d, sub))
-print('O produto custa {}. com 5% de desconto custa {}'.format(n1,sub))
+print('Bem vindo, compras acima de 10 unidades com 10% de desconto e acima 20 unidades com 20% de desconto!')
+
+n1 = eval(input('digite a quantidade de produtos que deseja:'))
+
+n2 = 20
+desc10 = 0.1
+desc20 = 0.2
+m = n2 * n1
+
+
+
+if n1 >= 10 < 20:
+    valor_final = 10 * n1 * (1 - desc10)
+
+elif n1 > 10 <= 20:
+    valor_final = 10 * n1 * (1 - desc20)
+
+else:
+    valor_final = 10 * n1
+
+print(f'A quantidade {n1} deste produto custa R${valor_final}')
+
