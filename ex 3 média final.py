6425216b1409436eef7e607fe31610ba7b5a5aad
DanielNathalia <77645774+DanielNathalia@users.noreply.github.com>
2024-05-17 13:32:54 -0300

    Update ex 3 média final.py

--- ex 3 média final.py
+++ ex 3 média final.py
@@ -3,4 +3,12 @@ nota1 = float(input('Informe a nota 1:'));
 nota2 = float(input('Informe a nota 2:'));
 soma = (nota1 + nota2);
 media = soma / 2
+
 print('A média das notas é {:.1f}'.format(media));
+if media >= 7.0:
+    print(f'Parabéns {nome}, você aprovado!')
+elif media >= 5.0:
+    print('Você está em recuperação!')
+
+else:
+    print('Você foi reprovado!')
